fa3092ca3eb9dbc1ddb4c511864bb2610ef0d767
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-02-01 19:10:51 +0200

    add loadmore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,50 +13,76 @@ const BASE_URL = "https://api.unsplash.com/search/photos";
 const clientID = "eweU7n7QNHGPet9x6rguFqq5agNu-FnnqAkMJV9TwHY";
 
 export const App = () => {
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
   const [articles, setArticles] = useState({
     items: [],
     loading: false,
     error: false,
   });
 
-  const notify = () => toast("Here is your toast.");
-  const searchArticles = async (query) => {
-    try {
-      setArticles({
-        items: [],
-        loading: true,
-        error: false,
-      });
-      const queryParams = {
-        client_id: clientID,
-        query: `${query}`,
-
-        orientation: "landscape",
-        page: 1,
-      };
-
-      const response = await axios.get(BASE_URL, { params: queryParams });
-
-      setArticles((prevArticles) => ({
-        ...prevArticles,
-        items: response.data.results,
-      }));
-    } catch (error) {
-      setArticles((prevArticles) => ({ ...prevArticles, error: true }));
-    } finally {
-      setArticles((prevArticles) => ({ ...prevArticles, loading: false }));
-    }
+  const searchArticles = async (newQuery) => {
+    setQuery(newQuery);
+    setPage(1);
+    setArticles({
+      items: [],
+      loading: false,
+      error: false,
+    });
   };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+
+    async function fetchData() {
+      try {
+        setArticles((prevArticles) => ({
+          ...prevArticles,
+          loading: true,
+          error: false,
+        }));
+
+        const queryParams = {
+          client_id: clientID,
+          query: `${query}`,
+          page: `${page}`,
+          per_page: 10,
+        };
+
+        const response = await axios.get(BASE_URL, { params: queryParams });
+
+        setArticles((prevArticles) => {
+          return {
+            ...prevArticles,
+            items: [...prevArticles.items, ...response.data.results],
+          };
+        });
+      } catch (error) {
+        setArticles((prevArticles) => ({ ...prevArticles, error: true }));
+      } finally {
+        setArticles((prevArticles) => ({ ...prevArticles, loading: false }));
+      }
+    }
+
+    fetchData();
+  }, [query, page]);
 
   return (
     <>
-      <SearchBar onSearch={searchArticles}></SearchBar>
-      {articles.loading && <Loader load={articles.loading} />}
+      <SearchBar onSearch={searchArticles} />
       {articles.error && <ErrorMessage />}
       {articles.items.length > 0 && <ImageGallery items={articles.items} />}
-      <Toaster />
-
-      <LoadMoreBtn></LoadMoreBtn>
+      {articles.loading && <Loader load={articles.loading} />}
+      {articles.items.length > 0 && !articles.loading && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
+      <Toaster position="bottom-center" />
 
       <ImageModal></ImageModal>
     </>
--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,5 +1,9 @@
 import css from "./LoadMoreBtn.module.css";
 
-export const LoadMoreBtn = () => {
-  return <div></div>;
+export const LoadMoreBtn = ({ onClick }) => {
+  return (
+    <div>
+      <button onClick={onClick}>Load more</button>
+    </div>
+  );
 };
