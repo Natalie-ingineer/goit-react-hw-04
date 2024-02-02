b1b104389bffc96cd42f5a66b02d6a901116d3a3
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-02-02 15:27:56 +0200

    update modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,20 +7,6 @@ import { LoadMoreBtn } from "./LoadMoreBtn/LoadMoreBtn";
 import { Loader } from "./Loader/Loader";
 import { Toaster } from "react-hot-toast";
 import { fetchArticles } from "../api";
-import Modal from "react-modal";
-
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
-// // // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
-Modal.setAppElement("#root");
 
 export const App = () => {
   const [query, setQuery] = useState("");
@@ -30,22 +16,6 @@ export const App = () => {
   const [error, setError] = useState(false);
 
   // const [total_pages, setTotalPages] = useState(1);
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  // let subtitle;
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  // function afterOpenModal() {
-  //   // references are now sync'd and can be accessed.
-  //   subtitle.style.color = "#f00";
-  // }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
 
   const searchArticles = async (newQuery) => {
     setQuery(`${Date.now()}/${newQuery}`);
@@ -86,73 +56,12 @@ export const App = () => {
     <>
       <SearchBar onSearch={searchArticles} />
       {error && <ErrorMessage />}
-      {articles.length > 0 && (
-        <ImageGallery items={articles} onClickOpenModal={openModal} />
-      )}
+      {articles.length > 0 && <ImageGallery items={articles} />}
       {loading && <Loader load={loading} />}
       {articles.length > 0 && !loading && (
         <LoadMoreBtn onClick={handleLoadMore} />
       )}
       <Toaster position="bottom-center" />
-
-      <div>
-        {/* <button onClick={openModal}>Open Modal</button> */}
-        <Modal
-          isOpen={modalIsOpen}
-          // onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
-          {/* <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2> */}
-          <button onClick={closeModal}>close</button>
-          <div>I am a modal</div>
-        </Modal>
-      </div>
     </>
   );
 };
-
-// ReactModal.setAppElement("#main");
-
-// class ExampleApp extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       showModal: false,
-//     };
-
-//     this.handleOpenModal = this.handleOpenModal.bind(this);
-//     this.handleCloseModal = this.handleCloseModal.bind(this);
-//   }
-
-//   handleOpenModal() {
-//     this.setState({ showModal: true });
-//   }
-
-//   handleCloseModal() {
-//     this.setState({ showModal: false });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.handleOpenModal}>Trigger Modal</button>
-//         <ReactModal
-//           isOpen={this.state.showModal}
-//           contentLabel="onRequestClose Example"
-//           onRequestClose={this.handleCloseModal}
-//           className="Modal"
-//           overlayClassName="Overlay"
-//         >
-//           <p>Modal text!</p>
-//           <button onClick={this.handleCloseModal}>Close Modal</button>
-//         </ReactModal>
-//       </div>
-//     );
-//   }
-// }
-
-// const props = {};
-
-// ReactDOM.render(<ExampleApp {...props} />, document.getElementById("main"));
--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,79 +1,45 @@
 import css from "./ImageModal.module.css";
-// import { openModal, afterOpenModal, closeModal } from "../App";
-// ReactModal.setAppElement("#main");
+import { useState } from "react";
+import Modal from "react-modal";
 
-// class ExampleApp extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       showModal: false,
-//     };
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
-//     this.handleOpenModal = this.handleOpenModal.bind(this);
-//     this.handleCloseModal = this.handleCloseModal.bind(this);
-//   }
+// Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement("#root");
 
-//   handleOpenModal() {
-//     this.setState({ showModal: true });
-//   }
+// ReactDOM.render(<App />, appElement);
 
-//   handleCloseModal() {
-//     this.setState({ showModal: false });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.handleOpenModal}>Trigger Modal</button>
-//         <ReactModal
-//           isOpen={this.state.showModal}
-//           contentLabel="onRequestClose Example"
-//           onRequestClose={this.handleCloseModal}
-//           className="Modal"
-//           overlayClassName="Overlay"
-//         >
-//           <p>Modal text!</p>
-//           <button onClick={this.handleCloseModal}>Close Modal</button>
-//         </ReactModal>
-//       </div>
-//     );
-//   }
-// }
-
-// const props = {};
-
-// ReactDOM.render(<ExampleApp {...props} />, document.getElementById("main"));
 export const ImageModal = ({ photo, description }) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <div>
-      <img src={photo} alt={description} />
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal}>close</button>
+        <img src={photo} alt={description} />
+      </Modal>
     </div>
   );
 };
-
-// // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
-
-//   return (
-//     <div>
-//       <button onClick={openModal}>Open Modal</button>
-//       <Modal
-//         isOpen={modalIsOpen}
-//         onAfterOpen={afterOpenModal}
-//         onRequestClose={closeModal}
-//         style={customStyles}
-//         contentLabel="Example Modal"
-//       >
-//         <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
-//         <button onClick={closeModal}>close</button>
-//         <div>I am a modal</div>
-//         <form>
-//           <input />
-//           <button>tab navigation</button>
-//           <button>stays</button>
-//           <button>inside</button>
-//           <button>the modal</button>
-//         </form>
-//       </Modal>
-//     </div>
-//   );
-// }
